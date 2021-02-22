366aa911cb4554ae856bd8ba23d010161f907a71
Tyler Beringer <99tylerberinger@gmail.com>
2021-02-21 20:46:31 -0500

    Started rework to have GUI with Pygame

--- src/main.py
+++ src/main.py
@@ -1,84 +1,51 @@
-from random import choice
+import pygame
+pygame.init()
 
 
-def get_decision(question, options):
-	for i in range(len(options)):
-		print(f"{i}. '{options[i]}'")
-	print(question)
-	# Get decision
-	while True:
-		# - make sure it's a number
-		try:
-			decision = int(input("Enter number: "))
-		except ValueError:
-			continue
-		# - make sure it's valid
-		if decision >= 0 and decision < len(options):
-			break
-	return options[decision]
+class Question():
+
+	def __init__(self):
+		self.font = pygame.font.SysFont(None, 150)
+		self.label = self.font.render("ENTER", True, color_text)
+		self.label_rect = self.label.get_rect(center=(width / 2, height / 2))
+
+	def update(self):
+		self.label = self.font.render(self.get_new_question(), True, color_text)
+		self.label_rect = self.label.get_rect(center=(width / 2, height / 2))
+
+	def get_new_question(self):
+		return "?"
 
 
-topic_options = ["addition", "subtraction", "multiplication", "division"]
-difficulty_options = ["easy", "medium"]
+color_background = (255, 255, 255)
+color_text = (32, 32, 32)
 
-print("\n----")
-print("noob-math-practicer")
-topic = get_decision("Which one do you want to practice?", topic_options)
-print("----")
-difficulty = get_decision("Which difficulty do you want?", difficulty_options)
-print("----\n")
+width = 420
+height = 180
 
-# Factor in difficulty
-range = None
-if difficulty == difficulty_options[0]:
-	range = [2, 5, 10, 11]
-elif difficulty == difficulty_options[1]:
-	range = [3, 4, 6, 7, 8, 9, 12]
+screen = pygame.display.set_mode((width, height))
+pygame.display.set_caption("noob-math-practicer")
 
-# Start quiz
-correct_answers = 0
-while correct_answers < 10:
-	# make the constants
-	constant1 = choice(range)
-	constant2 = choice(range)
-	# build the question/answer
-	question = None
-	answer = None
-	# 'addition'
-	if topic == topic_options[0]:
-		question = f"{constant1}+{constant2}="
-		answer = constant1 + constant2
-	# 'subtraction'
-	if topic == topic_options[1]:
-		if constant2 > constant1:
-			i = constant1
-			constant1 = constant2
-			constant2 = i
-		question = f"{constant1}-{constant2}="
-		answer = constant1 - constant2
-	# 'multiplication'
-	if topic == topic_options[2]:
-		question = f"{constant1}*{constant2}="
-		answer = constant1 * constant2
-	# 'division'
-	if topic == topic_options[3]:
-		i = constant1 * constant2
-		question = f"{i}/{constant2}="
-		answer = constant1
-	# Get guess
-	while True:
-		# - make sure it's a number
-		while True:
-			try:
-				guess = int(input(question))
-				break
-			except ValueError:
-				print("error: number is required")
-		# - make sure it's valid
-		if guess == answer:
-			correct_answers += 1
-			print("Correct\n")
-			break
-		else:
-			print("WRONG!")
+question = Question()
+
+
+running = True
+while running:
+
+	# EVENTS
+	for event in pygame.event.get():
+
+		# 'quit'
+		if event.type == pygame.QUIT:
+			running = False
+
+		# 'keydown'
+		elif event.type == pygame.KEYDOWN:
+			if event.key == pygame.K_RETURN:
+				question.update()
+
+	# DISPLAY
+	screen.fill(color_background)
+	screen.blit(question.label, question.label_rect)
+	pygame.display.flip()
 
