83c810ac7a95b8dc85a0ba7984f9403944a0cb70
Tyler Beringer <99tylerberinger@gmail.com>
2021-02-25 13:20:35 -0500

    Changed Question to Asker, and started storing answer

--- src/main.py
+++ src/main.py
@@ -2,14 +2,15 @@ import pygame
 from random import choice, randint
 
 
-class Question():
+class Asker():
 
 	def __init__(self):
 
-		self.current_question = "ENTER"
+		# [question, answer]
+		self.current_ask = ["•••", None]
 
 		self.font = pygame.font.SysFont(None, 150)
-		self.label = self.font.render(self.current_question, True, color_text)
+		self.label = self.font.render(self.current_ask[0], True, color_text)
 		self.label_rect = self.label.get_rect(center=(width / 2, height / 2))
 
 		self.topics = ["addition", "subtraction", "multiplication", "division"]
@@ -18,22 +19,24 @@ class Question():
 		self.constants = [3, 4, 6, 7, 8, 9, 12]
 
 	def update(self):
-		self.label = self.font.render(self.get_new_question(), True, color_text)
+		self.label = self.font.render(self.get_new_ask()[0], True, color_text)
 		self.label_rect = self.label.get_rect(center=(width / 2, height / 2))
+		print(f"{self.current_ask[1]}")
 
-	def get_new_question(self):
+	def get_new_ask(self):
 
 		current_topic = self.previous_topic
 		while current_topic == self.previous_topic:
 			current_topic = self.topics[randint(0, len(self.topics) - 1)]
-		print(f"topic: {current_topic}")
+		print(f"current_topic: {current_topic}")
 
 		constant1 = choice(self.constants)
 		constant2 = choice(self.constants)
 
 		# Addition
 		if current_topic == self.topics[0]:
-			self.current_question = f"{constant1} + {constant2}"
+			self.current_ask[0] = f"{constant1} + {constant2}"
+			self.current_ask[1] = f"{constant1 + constant2}"
 		# Subtraction
 		elif current_topic == self.topics[1]:
 			# - prevent answer from being 0
@@ -44,18 +47,21 @@ class Question():
 				i = constant1
 				constant1 = constant2
 				constant2 = i
-			self.current_question = f"{constant1} - {constant2}"
+			self.current_ask[0] = f"{constant1} - {constant2}"
+			self.current_ask[1] = f"{constant1 - constant2}"
 		# Multiplication
 		elif current_topic == self.topics[2]:
-			self.current_question = f"{constant1} x {constant2}"
+			self.current_ask[0] = f"{constant1} x {constant2}"
+			self.current_ask[1] = f"{constant1 * constant2}"
 		# Division
 		elif current_topic == self.topics[3]:
 			i = constant1 * constant2
-			self.current_question = f"{i} / {constant2}"
+			self.current_ask[0] = f"{i} / {constant2}"
+			self.current_ask[1] = f"{i // constant2}"
 
 		self.previous_topic = current_topic
 
-		return self.current_question
+		return self.current_ask
 
 
 pygame.init()
@@ -69,7 +75,7 @@ height = 180
 screen = pygame.display.set_mode((width, height))
 pygame.display.set_caption("noob-math-practicer")
 
-question = Question()
+asker = Asker()
 
 
 running = True
@@ -85,10 +91,10 @@ while running:
 		# 'keydown'
 		elif event.type == pygame.KEYDOWN:
 			if event.key == pygame.K_RETURN:
-				question.update()
+				asker.update()
 
 	# DISPLAY
 	screen.fill(color_background)
-	screen.blit(question.label, question.label_rect)
+	screen.blit(asker.label, asker.label_rect)
 	pygame.display.flip()
 
