3fd9a32a92b82e3d31501e3626afbee6790ee638
Tyler Beringer <99tylerberinger@gmail.com>
2021-02-27 18:41:32 -0500

    Made project be one class

--- src/main.py
+++ src/main.py
@@ -6,18 +6,46 @@ class Asker():
 
 	def __init__(self):
 
+		self.color_background = (255, 255, 255)
+		self.color_text = (32, 32, 32)
+		self.width = 420
+		self.height = 180
+
+		pygame.init()
+		self.screen = pygame.display.set_mode((self.width, self.height))
+		pygame.display.set_caption("noob-math-practicer")
+
+		self.topics = ["addition", "subtraction", "multiplication", "division"]
+		self.previous_topic = self.topics[0]
+		self.constants = [3, 4, 6, 7, 8, 9, 12]
+
 		# [question, answer]
 		self.current_ask = ["1 + 1", "2"]
 		self.current_ask_index = 0
 
 		self.font = pygame.font.SysFont(None, 150)
-		self.label = self.font.render(self.current_ask[self.current_ask_index], True, color_text)
-		self.label_rect = self.label.get_rect(center=(width // 2, height // 2))
+		self.label = self.font.render(self.current_ask[self.current_ask_index], True, self.color_text)
+		self.label_rect = self.label.get_rect(center=(self.width // 2, self.height // 2))
 
-		self.topics = ["addition", "subtraction", "multiplication", "division"]
-		self.previous_topic = self.topics[0]
+		running = True
+		while running:
 
-		self.constants = [3, 4, 6, 7, 8, 9, 12]
+			# EVENTS
+			for event in pygame.event.get():
+
+				# 'quit'
+				if event.type == pygame.QUIT:
+					running = False
+
+				# 'keydown'
+				elif event.type == pygame.KEYDOWN:
+					if event.key == pygame.K_RETURN:
+						self.update()
+
+			# DISPLAY
+			self.screen.fill(self.color_background)
+			self.screen.blit(self.label, self.label_rect)
+			pygame.display.flip()
 
 	def update(self):
 
@@ -25,15 +53,15 @@ class Asker():
 		if self.current_ask_index == 0:
 			self.current_ask_index = 1
 		else:
-			self.update_ask()
+			self.change_ask()
 			self.current_ask_index = 0
 			print(f"current_ask: [{self.current_ask[0]}] [{self.current_ask[1]}]")
 
-		self.label = self.font.render(self.current_ask[self.current_ask_index], True, color_text)
-		self.label_rect = self.label.get_rect(center=(width // 2, height // 2))
+		self.label = self.font.render(self.current_ask[self.current_ask_index], True, self.color_text)
+		self.label_rect = self.label.get_rect(center=(self.width // 2, self.height // 2))
 
-	def update_ask(self):
-
+	def change_ask(self):
+		
 		current_topic = self.previous_topic
 		while current_topic == self.previous_topic:
 			current_topic = self.topics[randint(0, len(self.topics) - 1)]
@@ -60,7 +88,6 @@ class Asker():
 		elif current_topic == self.topics[2]:
 			self.current_ask[0] = f"{constant1} x {constant2}"
 			self.current_ask[1] = f"{constant1 * constant2}"
-		# Division
 		elif current_topic == self.topics[3]:
 			i = constant1 * constant2
 			self.current_ask[0] = f"{i} / {constant2}"
@@ -69,37 +96,5 @@ class Asker():
 		self.previous_topic = current_topic
 
 
-pygame.init()
-
-color_background = (255, 255, 255)
-color_text = (32, 32, 32)
-
-width = 420
-height = 180
-
-screen = pygame.display.set_mode((width, height))
-pygame.display.set_caption("noob-math-practicer")
-
-asker = Asker()
-
-
-running = True
-while running:
-
-	# EVENTS
-	for event in pygame.event.get():
-
-		# 'quit'
-		if event.type == pygame.QUIT:
-			running = False
-
-		# 'keydown'
-		elif event.type == pygame.KEYDOWN:
-			if event.key == pygame.K_RETURN:
-				asker.update()
-
-	# DISPLAY
-	screen.fill(color_background)
-	screen.blit(asker.label, asker.label_rect)
-	pygame.display.flip()
+Asker()
 
