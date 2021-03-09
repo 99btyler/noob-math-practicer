c670b0054a9c70e6885f7acfe99b101fb5c2d67e
Tyler Beringer <99tylerberinger@gmail.com>
2021-03-09 14:48:53 -0500

    Renamed some variables and organized code

--- src/main.py
+++ src/main.py
@@ -13,11 +13,11 @@ class Asker():
 		self.height = 180
 
 		self.topics = ["addition", "subtraction", "multiplication", "division"]
-		self.previous_topic = self.topics[0]
-		self.constants = [3, 4, 6, 7, 8, 9, 12]
+		self.current_topic = self.topics[0]
 
-		self.current_ask = ["1 + 1", "2"]
-		self.current_ask_index = 0
+		self.constants = [3, 4, 6, 7, 8, 9, 12]
+		self.current_question = ["1 + 1", "2"]
+		self.is_asking = True
 
 		# Pygame screen
 		pygame.init()
@@ -26,7 +26,7 @@ class Asker():
 
 		# Pygame widgets
 		self.font = pygame.font.SysFont(None, 150)
-		self.label = self.font.render(self.current_ask[self.current_ask_index], True, self.color_text)
+		self.label = self.font.render(self.current_question[0 if self.is_asking else 1], True, self.color_text)
 		self.label_rect = self.label.get_rect(center=(self.width // 2, self.height // 2))
 
 		# Pygame loop
@@ -43,59 +43,65 @@ class Asker():
 				# 'keydown'
 				elif event.type == pygame.KEYDOWN:
 					if event.key == pygame.K_RETURN:
-						self.update()
+						self.update_screen()
 
 			# - display
 			self.screen.fill(self.color_background)
 			self.screen.blit(self.label, self.label_rect)
 			pygame.display.flip()
 
-	def update(self):
+	def update_screen(self):
 
-		if self.current_ask_index == 0:
-			self.current_ask_index = 1
-		else:
-			self.change_ask()
-			self.current_ask_index = 0
-			print(f"current_ask: [{self.current_ask[0]}] [{self.current_ask[1]}]")
+		self.switch_current_question()
 
-		self.label = self.font.render(self.current_ask[self.current_ask_index], True, self.color_text)
+		self.label = self.font.render(self.current_question[0 if self.is_asking else 1], True, self.color_text)
 		self.label_rect = self.label.get_rect(center=(self.width // 2, self.height // 2))
+	
+	def switch_current_question(self):
 
-	def change_ask(self):
-		
-		current_topic = self.previous_topic
-		while current_topic == self.previous_topic:
-			current_topic = self.topics[random.randint(0, len(self.topics) - 1)]
-		print(f"\ncurrent_topic: {current_topic}")
+		self.is_asking = not self.is_asking
+
+		if self.is_asking:
+
+			# update current_topic
+			new_topic = self.current_topic
+			while new_topic == self.current_topic:
+				new_topic = self.topics[random.randint(0, len(self.topics) - 1)]
+			self.current_topic = new_topic
+			print(f"\ncurrent_topic: {self.current_topic}")
+
+			# update current_question
+			self.current_question = self.get_new_question()
+			print(f"current_question: [{self.current_question[0]}] [{self.current_question[1]}]")
+
+	def get_new_question(self):
+
+		new_question = ["question", "answer"]
 
 		constant1 = random.choice(self.constants)
 		constant2 = random.choice(self.constants)
 
-		# Addition
-		if current_topic == self.topics[0]:
-			self.current_ask[0] = f"{constant1} + {constant2}"
-			self.current_ask[1] = f"{constant1 + constant2}"
-		# Subtraction
-		elif current_topic == self.topics[1]:
+		if self.current_topic == "addition":
+			new_question[0] = f"{constant1} + {constant2}"
+			new_question[1] = f"{constant1 + constant2}"
+		elif self.current_topic == "subtraction":
 			# prevent answer from being 0
 			while constant1 == constant2:
 				constant1 = random.choice(self.constants)
 			# prevent answer from being negative
 			if constant2 > constant1:
 				constant1, constant2 = constant2, constant1
-			self.current_ask[0] = f"{constant1} - {constant2}"
-			self.current_ask[1] = f"{constant1 - constant2}"
-		# Multiplication
-		elif current_topic == self.topics[2]:
-			self.current_ask[0] = f"{constant1} x {constant2}"
-			self.current_ask[1] = f"{constant1 * constant2}"
-		elif current_topic == self.topics[3]:
+			new_question[0] = f"{constant1} - {constant2}"
+			new_question[1] = f"{constant1 - constant2}"
+		elif self.current_topic == "multiplication":
+			new_question[0] = f"{constant1} x {constant2}"
+			new_question[1] = f"{constant1 * constant2}"
+		elif self.current_topic == "division":
 			i = constant1 * constant2
-			self.current_ask[0] = f"{i} / {constant2}"
-			self.current_ask[1] = f"{i // constant2}"
+			new_question[0] = f"{i} / {constant2}"
+			new_question[1] = f"{i // constant2}"
 
-		self.previous_topic = current_topic
+		return new_question
 
 
 Asker()
